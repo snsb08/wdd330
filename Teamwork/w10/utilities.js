32e6994f83ea9e753ac7402f7c4cfbc42f8fba0f
snsb08 <76939844+snsb08@users.noreply.github.com>
2022-11-19 21:40:28 -0500

    week 10 assignments

---
+++ Teamwork/w10/utilities.js
@@ -0,0 +1,29 @@
+// export function getJSON(url){ 
+//     fetch(url).then(response => response.json())
+//     .then(json =>{
+//         console.log(json);
+//     }).catch(function(error) {
+//         console.log(error);
+//     });
+
+// };
+
+export function getJSON(url) {
+    return fetch(url)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          //console.log(response.json());
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
